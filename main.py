9e71cf473bcc2b61be67bd0695d7b0393e714ce2
gracejoh <ganwynj@icloud.com>
2020-07-14 16:58:27 +0000

    .

---
+++ main.py
@@ -0,0 +1,7 @@
+name = "grace"
+favsong = "pink + white"
+vacation = "italy"
+hobby = "playing flute"
+today = "july fourteenth"
+
+print ("my name is " + name + ". my favorite song is " + favsong + ". I would like to vacation in " + vacation + ". my hobby is " + hobby + ". today is " + today + "." )
